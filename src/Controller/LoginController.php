1d3df5bf96a32f8f9043ddedc94ff627c03f4147
Benoit LAGUET <laguet.benoit@gmail.com>
2025-08-30 11:57:08 +0200

    refactor: clean up logout method implementation in LoginController

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\BrowserKit\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -26,6 +25,7 @@ final class LoginController extends AbstractController
     #[Route('/logout', name: 'app_logout', methods: ['GET'])]
     public function logout(): never
     {
-        throw new \Exception('Logout not implemented');
+        // Intercepted by the firewall
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
